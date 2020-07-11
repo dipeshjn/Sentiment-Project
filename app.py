1f0c108e648ff00b2d2ee5bfa5e2d0c38e7b044e
dipeshjn <38787731+dipeshjn@users.noreply.github.com>
2020-07-11 15:04:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict',methods=['GET'])
 def predict():
     '''
     For rendering results on HTML GUI
